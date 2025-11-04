4523eaad9586998dbf904167933c40d3ca71ac91
a930853 <930853@unizar.es>
2025-11-04 16:01:23 +0100

    faltan solo comentarios del TAD

--- Prac3/mainPruebas.cpp
+++ Prac3/mainPruebas.cpp
@@ -4,10 +4,32 @@ using namespace std;
 
 int main() {
     colecInterdep<int,char> ci;
-    crear(ci);
-    anyadirIndependiente(ci,4,'c');
-    anyadirIndependiente(ci,5,'f');
-    anyadirIndependiente(ci,2,'a');
-    anyadirIndependiente(ci,5,'f');
+    int id,idSup;
+    char v;
+    bool error,esDep;
+    unsigned NumDep;
+      crear(ci);
+      tamanyo(ci);
+      esVacia(ci);
+      existe( id,ci);
+      existeDependiente( id,ci);
+      existeIndependiente( id,ci);
+      anyadirIndependiente(ci,  id,  v);
+      anyadirDependiente(ci,  id,  v,  idSup);
+      hacerDependiente(ci,  id,  idSup);
+      hacerIndependiente(ci,  id);
+      actualizarVal(ci,  id,  v,  error);
+      obtenerVal( id,ci,  v,  error);
+      obtenerSupervisor( id, ci,  idSup,  error);
+      obtenerNumDependientes( id, ci,  NumDep,  error);
+      borrar( id,  ci);
+      iniciarIterador( ci);
+      existeSiguiente(ci);
+      siguienteIdent(ci,  id,  error);
+      siguienteVal(ci,  v,  error);
+      siguienteDependiente(ci,  esDep,  error);
+      siguienteSuperior(ci,  idSup,  error);
+      siguienteNumDependientes(ci,  NumDep,  error);
+      avanza( ci,  error);
     return 0;
 }
