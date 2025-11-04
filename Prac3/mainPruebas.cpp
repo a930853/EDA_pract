fc005302bc0c0cd5964ff99deb06c67b019b1a61
a928927 <928927@unizar.es>
2025-11-04 16:48:52 +0100

    instr. A y entradaPrueba.txt

--- Prac3/mainPruebas.cpp
+++ Prac3/mainPruebas.cpp
@@ -4,14 +4,68 @@
 
 #include "evento.hpp"
 #include "colecInterdep.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 int main() {
-    colecInterdep<int,char> ci;
-    crear(ci);
-    anyadirIndependiente(ci,4,'c');
-    anyadirIndependiente(ci,5,'f');
-    anyadirIndependiente(ci,2,'a');
-    anyadirIndependiente(ci,5,'f');
-    return 0;
+    
+    colecInterdep<string,evento> ci;
+
+    crear(ci);  // creamos la colección vacía
+
+    ifstream f; 
+    f.open("entrada.txt"); 
+    if(!f.is_open()) {
+        cerr << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    string instruccion, salto; 
+    string nom;     // nombre
+    evento e;       // evento
+    string desc;    // descripción
+    string prioS;   // prioridad (string)
+    int prio;       // prioridad (int)
+    string tipoDep;  // tipo de dependencia (dep. o ind.)
+    string eventoSup;   // nombre del evento del cual depende el evento que se añade ("vacío" si es independiente)
+
+    while (f >> instruccion) {     
+	    getline(f,salto);   
+         
+	    if (instruccion == "A") {         
+            
+            // quizás hay que poner aquí un "existe()" o algo parecido ????????????????????
+            
+            getline(f,nom);
+            getline(f,desc);
+            getline(f,prioS);
+            prio = stoi(prioS);     // convertimos el string "prioS" en un int "prio"
+            getline(f,tipoDep);
+            getline(f,eventoSup);
+
+            crearEvento(desc,prio,e); // creamos el evento con la descripción y prioridad dadas
+
+            if(tipoDep == "DEPendiente") {
+                anyadirDependiente(ci,nom,e,eventoSup);
+
+            } else {                // poner un caso por si no pone es ni dep ni ind ??????????????????????????
+                anyadirIndependiente(ci,nom,e);
+
+                
+                // salida.txt
+
+            }
+
+
+            
+
+
+	    } else if (instruccion == "O") {         
+		//... sigue el programa 
+        }//... sigue el programa
+    }
+    f.close();
 }
