83d10fb42802898f928e792c56181c63c302c49f
a930853 <930853@unizar.es>
2025-11-05 15:07:34 +0100

    eventos hpp y cpp comentados

--- Prac3/evento.cpp
+++ Prac3/evento.cpp
@@ -3,24 +3,43 @@
 */
 #include "evento.hpp"
 
-
-void crearEvento(string descripcion, int prioridad, evento& e) {
+/* Modifica 'e' para que sea un evento compuesto con descripción 'descripcion' 
+* y con prioridad 'prioridad'.
+* Coste: Θ(1)
+*/
+void crearEvento(string descripcion, unsigned prioridad, evento& e) {
     e.descripcion = descripcion;
     e.prioridad = prioridad;
 }
 
+/* Dado un evento 'e' formado como (D,P), devuelve la cadena D, 
+* i.e. la descripción en el evento e.
+* Coste: Θ(1)
+*/
 string descripcion(const evento& e) {
     return e.descripcion;
 }
 
+/* Dado un evento 'e' formado como (D,P), modifica 'e' para que sea igual 
+* al evento compuesto como (nuevaD, P).
+* Coste: Θ(1)
+*/
 void cambiarDescripcion(evento& e,string nuevaD) {
     e.descripcion = nuevaD;
 }
 
-int suPrioridad(const evento& e) {
+/* Dado un evento 'e' formado como (D,P), devuelve P, 
+* i.e. la prioridad en el evento e.
+* Coste: Θ(1)
+*/
+unsigned suPrioridad(const evento& e) {
     return e.prioridad;
 }
 
-void cambiarPrioridad(evento& e,int nuevaP) {
+/* Dado un evento 'e' formado como (D,P) y un natural 'nuevaP', 
+* modifica 'e' para que sea igual al evento compuesto como (D, nuevaP).
+* Coste: Θ(1)
+*/
+void cambiarPrioridad(evento& e, unsigned nuevaP) {
     e.prioridad = nuevaP;
 }
--- Prac3/evento.hpp
+++ Prac3/evento.hpp
@@ -7,34 +7,52 @@
 #include<iostream>
 using namespace std;
 
-// PREDECLARACION DEL TAD contacto (inicio INTERFAZ)
+// PREDECLARACION DEL TAD evento (inicio INTERFAZ)
 
+/* Los valores del TAD evento representarán tuplas formadas como:
+* (descripción, prioridad) siendo la descripción una cadena y la prioridad un número natural.
+*/
 struct evento;
 
-void crearEvento(string descripcion, int prioridad, evento& e);
+/* Devuelve en 'e' un evento compuesto con descripción 'descripcion' y con prioridad 'prioridad'.
+*/
+void crearEvento(string descripcion, unsigned prioridad, evento& e);
 
+/* Dado un evento 'e' formado como (D,P), devuelve la cadena D, 
+* i.e. la descripción en el evento e.
+*/
 string descripcion(const evento& e);
 
+/* Dado un evento 'e' formado como (D,P), modifica 'e' para que sea igual 
+* al evento compuesto como (nuevaD, P).
+*/
 void cambiarDescripcion(evento& e,string nuevaD);
 
-int suPrioridad(const evento& e);
+/* Dado un evento 'e' formado como (D,P), devuelve P, 
+* i.e. la prioridad en el evento e.
+*/
+unsigned suPrioridad(const evento& e);
 
-void cambiarPrioridad(evento& e,int nuevaP);
+/* Dado un evento 'e' formado como (D,P) y un natural 'nuevaP', 
+* modifica 'e' para que sea igual al evento compuesto como (D, nuevaP).
+*/
+void cambiarPrioridad(evento& e,unsigned nuevaP);
 
 // FIN de la PREDECLARACION DEL TAD evento (fin INTERFAZ)
 
 // DECLARACION DEL TAD evento 
 
 struct evento {
-  friend void crearEvento(string descripcion, int prioridad, evento& e);
+  friend void crearEvento(string descripcion, unsigned prioridad, evento& e);
   friend string descripcion(const evento& e);
   friend void cambiarDescripcion(evento& e,string nuevaD);
-  friend int suPrioridad(const evento& e);
-  friend void cambiarPrioridad(evento& e,int nuevaP);
-  private:  //declaracion de la representacion interna del tipo
-            //... a completar COMPLETAR CON documentacion sobre la representacion interna ...
-    string descripcion;
-    int prioridad;
+  friend unsigned suPrioridad(const evento& e);
+  friend void cambiarPrioridad(evento& e,unsigned nuevaP);
+  private:   //declaracion de la representacion interna del tipo
+        // Los valores del TAD evento (tuplas (descripción, prioridad))
+        // se representan mediante un registro (struct) con dos campos.
+     string descripcion; // Campo para almacenar la descripción del evento.
+     unsigned prioridad;      // Campo para almacenar la prioridad (natural) del evento.
 };
 
 #endif
